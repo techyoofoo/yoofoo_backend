0e9d3b90ce4097790057fd3c4138731ae384acfe
abhaymcauuv <abhaymcauuv@gmail.com>
2019-11-12 18:07:35 +0530

    Role permission schema changes

--- role-permission/src/api/role-permission/index.js
+++ role-permission/src/api/role-permission/index.js
@@ -4,11 +4,10 @@ export const create = function (request, reply) {
     return new Promise(async (resolve, reject) => {
         try {
             RolePermissionSchema.find({
-                name: request.payload.name,
                 roleid: request.payload.roleid
             }).then(result => {
                 if (result.length !== 0)
-                    return resolve(reply.response({ _id: result[0]._id, Message: request.payload.name + " already exist with same role" }).code(200));
+                    return resolve(reply.response({ _id: result[0]._id, Message: "Permissions already exist with same role" }).code(200));
                 else {
                     const rolepermission = new RolePermissionSchema(request.payload)
                     rolepermission
@@ -57,7 +56,7 @@ export const updateRolePermissionById = function (request, reply) {
         try {
             await RolePermissionSchema.findOneAndUpdate({ _id: request.params.id }, request.payload, { new: false })
                 .then(result => {
-                    return resolve(reply.response({ Message: `${result.name} Updated Successfully` }).code(200));
+                    return resolve(reply.response({ Message: `Updated Successfully` }).code(200));
                 })
                 .catch(err => {
                     console.log(err.message)
@@ -115,6 +114,38 @@ export const findRolePermissionById = function (request, reply) {
         }
     });
 }
+
+
+export const removePermissionFromRolePermission = function (request, reply) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            await RolePermissionSchema.find({})
+                .then(result => {
+                    result.forEach((d, i) => {
+                        var index = d.permission.indexOf(request.params.id)
+                        if (index !== -1) {
+                            d.permission = d.permission.filter(x=>x !== request.params.id)
+                            RolePermissionSchema.findOneAndUpdate({ _id: d._id }, { permission: d.permission }, { new: false })
+                                .catch(err => {
+                                    console.log(err.message)
+                                });
+                        }
+                    })
+                    return resolve(reply.response(result).code(200));
+                })
+                .catch(err => {
+                    console.log(err.message)
+                    return resolve(reply.response(err.message).code(500));
+                });
+        }
+        catch (err) {
+            console.log(err.message)
+            return resolve(reply.response(err.message).code(500));
+        }
+    });
+}
+
+
 
 
 
--- role-permission/src/api/role-permission/routes.js
+++ role-permission/src/api/role-permission/routes.js
@@ -1,9 +1,10 @@
-import { create, getAll, updateRolePermissionById, deleteRolePermissionById, findRolePermissionById } from './index'
+import { create, getAll, updateRolePermissionById, deleteRolePermissionById, findRolePermissionById ,removePermissionFromRolePermission} from './index'
 
 export const rolepermissionroutes = [
     { method: 'POST', path: '/rouge/rolepermission/create', handler: create },
     { method: 'GET', path: '/rouge/rolepermission/get', handler: getAll },
     { method: 'PUT', path: '/rouge/rolepermission/update/{id}', handler: updateRolePermissionById },
     { method: 'DELETE', path: '/rouge/rolepermission/delete/{id}', handler: deleteRolePermissionById },
-    { method: 'GET', path: '/rouge/rolepermission/get/{id}', handler: findRolePermissionById }
+    { method: 'GET', path: '/rouge/rolepermission/get/{id}', handler: findRolePermissionById },
+    { method: 'DELETE', path: '/rouge/rolepermission/deletepermission/{id}', handler: removePermissionFromRolePermission }
 ]
